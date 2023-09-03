762617da23513bde6a4571cd72cc6775a4569340
Adhiban1 <siddhu1131@gmail.com>
2023-09-03 16:48:06 +0530

    changed

--- datasplit.py
+++ datasplit.py
@@ -1,8 +1,10 @@
 import os
 import pandas as pd
-import shutil
 from zipfile import ZipFile
+import random
+from time import time
 
+# Extraction dataset.zip
 if not os.path.exists('dataset'):
     with ZipFile('dataset.zip', 'r') as f:
         f.extractall()
@@ -10,7 +12,9 @@ if not os.path.exists('dataset'):
 else:
     print('Folder "dataset" already exist')
 
+# This will return all the files locations from the given dir
 def all_files(folder):
+    'This will return all the files locations from the given dir'
     files = []
     for i,_,k in os.walk(folder):
         if len(k) != 0:
@@ -18,50 +22,134 @@ def all_files(folder):
                 files.append(os.path.join(i, l))
     return files
 
-data = [['Electric_Vehicle_Population_Data.csv'], 
-        ['IEA-EV-dataEV_salesHistoricalCars.csv'],
-        ['RS_Session_257_AU_2368_A.csv', 'RS_Session_256_AU_2673_2.i.csv', 
-         'RS_Session_256_AU_2673_1.csv', 'RS_Session_258_AU_429_1.csv'],
-        ['EV_India.csv', 'EV_cars_India_2023.csv'],
-        ['4-wheeler-EV-cardekho.csv', '2-wheeler-EV-bikewale.csv', 
-         '4-wheeler-EV-carwale.csv', 'EV_CARS _INDIA.csv']]
-
-data_count = []
-links = []
-
 files = all_files('dataset')
 
-for fs in data:
-    link = []
-    r, c = 0, 0
-    for f in fs:
-        for loc in files:
-            if f in loc:
-                link.append(loc)
-                df = pd.read_csv(loc)
-                r += df.shape[0]
-                c += df.shape[1]
-                break
-    links.append(link)
-    data_count.append([r, c])
+# change detection
+with open('files.csv', 'r') as f:
+    pre_files = f.read().split('\n')
 
-for i,j in enumerate(data_count):
-    print(f'Member {i+1} | {j[0]} | {j[1]}')
-
-for i in links:
-    print(i)
-
-if not os.path.exists('Data Splitup') and not os.path.exists('zipfiles'):
-    os.mkdir('Data Splitup')
-    os.mkdir('zipfiles')
-    for i in range(5):
-        zf = ZipFile(f'zipfiles/member{i+1}.zip', 'w')
-        os.mkdir(f'Data Splitup/member{i+1}')
-        for loc in links[i]:
-            shutil.copy(loc, f'Data Splitup/member{i+1}/{os.path.basename(loc)}')
-            zf.write(loc, os.path.basename(loc))
-        zf.close()
+if pre_files == files:
+    print('No change in files')
+    new_files = False
 else:
-    print('"Data Splitup" or "zipfiles" already exist')
+    new_files = True
+    print('New files:')
+    for i in files:
+        if i not in pre_files:
+            print(i)
 
-print('Success')
+with open('files.csv', 'w') as f:
+    f.write('\n'.join(files))
+
+# Details of csv files
+def csv_details(files):
+    'This gets list or csv file\'s locations \
+    and returns them as dfs, and details of rows, columns and cells'
+    dfs = {}
+    data = []
+    for f in files:
+        df = pd.read_csv(f)
+        name = os.path.basename(f)
+        dfs[name] = df
+        data.append([name, df.shape[0], df.shape[1], 
+                     df.shape[0]*df.shape[1]])
+    details = pd.DataFrame(
+        data, 
+        columns=['filename', 'rows', 'columns', 'cells']
+    ).sort_values('cells').reset_index(drop=True)
+
+    no_duplicate_filenames = len(files) == len(dfs)
+
+    return dfs, details, no_duplicate_filenames
+
+dfs, details, no_duplicate_filenames = csv_details(files)
+print(details)
+print(no_duplicate_filenames)
+
+# dataset splitup to 5 team members
+d = {}
+for i in details.values:
+    d[i[3]] = i[0]
+
+print(f'{details["cells"].sum()/5 = }')
+print(f'{(details["cells"].sum()-1914778)/5 = }')
+
+cells = list(details["cells"])
+
+def cells_split():
+    while True:
+        l = []
+        for _ in range(5):
+            l.append(random.randint(3, len(cells)//2))
+        if sum(l) == len(cells):
+            break
+    return l
+
+def split_dataset():
+    cells = list(details["cells"])
+    random.shuffle(cells)
+    rearrage = []
+    k = 0
+    for i in cells_split():
+        l = []
+        for _ in range(i):
+            l.append(cells[k])
+            k += 1;
+        rearrage.append(l)
+    return rearrage
+
+def even_splitup(min_rows):
+    start = time()
+    while True:
+        splitup = split_dataset()
+        temp = []
+        sum_val = []
+        for i in splitup:
+            temp.append(min_rows < sum(i))
+            sum_val.append(sum(i))
+        print(f'\r({round(time()-start)}s) {sum_val = }    ', end='', flush=True)
+        if all(temp) or time()-start > 60:
+            if time()-start > 60:
+                print('[Time out]')
+            else:
+                print('[Findout]')
+            break
+    print()
+
+    splitup_name = []
+    for i in splitup:
+        temp = []
+        for j in i:
+            temp.append(d[j])
+        splitup_name.append(temp)
+    return splitup, sum_val, splitup_name
+
+if new_files:
+    splitup, sum_val, splitup_name = even_splitup(5000)
+
+    print(f'{splitup = }')
+    print(f'{splitup_name = }')
+
+    with open('splitup.txt', 'w') as f:
+        f.write(f'{splitup = }\n{splitup_name = }')
+
+# seperate dataset in folders
+m = 0
+if not os.path.exists('splitup'):
+    os.mkdir('splitup')
+    for i in splitup_name:
+        m += 1
+        os.mkdir(f'splitup/member{m}')
+        for j in i:
+            dfs[j].to_csv(f'splitup/member{m}/{j}', index=False)
+elif new_files:
+    os.system('rm -rf splitup')
+    os.mkdir('splitup')
+    for i in splitup_name:
+        m += 1
+        os.mkdir(f'splitup/member{m}')
+        for j in i:
+            dfs[j].to_csv(f'splitup/member{m}/{j}', index=False)
+    print('"splitup" folder modified')
+else:
+    print('"splitup" no change')
